b48b63ced0a2d20beb95963cb98d6d7375c96fdd
Atiqur Rahaman <toatikbd@gmail.com>
2021-05-27 16:33:17 +0600

    Admin Template Setup

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes(['register'=> false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/backpanel', 'backpanel.dashboard.index');
