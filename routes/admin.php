042c593f6de0ae5569518b1ee3080939641ec73b
atiqur <toatikbd@gmail.com>
2021-07-09 23:55:07 +0600

    create role controller and update user routes

--- routes/admin.php
+++ routes/admin.php
@@ -6,9 +6,12 @@ Route::view('/backpanel', 'backpanel.dashboard.index')->name('backpanel.dashboar
 //Route::view('/back', 'welcomeback');
 //user routes
 // Route::get('/backpanel/users', 'User\UserController@index')->name('user.index');
-Route::get('/backpanel/users', [App\Http\Controllers\User\UserController::class, 'index'])->name('user.index');
-Route::get('/backpanel/users/create', [App\Http\Controllers\User\UserController::class, 'create'])->name('user.create');
-Route::post('/backpanel/users/create', [App\Http\Controllers\User\UserController::class, 'store'])->name('user.store');
-Route::get('/backpanel/users/{user}/edit', [App\Http\Controllers\User\UserController::class, 'edit'])->name('user.edit');
-Route::put('/backpanel/users/{user}/edit', [App\Http\Controllers\User\UserController::class, 'update'])->name('user.update');
-Route::delete('/backpanel/users/{user}/edit', [App\Http\Controllers\User\UserController::class, 'destroy'])->name('user.destroy');
+
+// Route::get('/backpanel/users', [App\Http\Controllers\User\UserController::class, 'index'])->name('user.index');
+// Route::get('/backpanel/users/create', [App\Http\Controllers\User\UserController::class, 'create'])->name('user.create');
+// Route::post('/backpanel/users/create', [App\Http\Controllers\User\UserController::class, 'store'])->name('user.store');
+// Route::get('/backpanel/users/{user}/edit', [App\Http\Controllers\User\UserController::class, 'edit'])->name('user.edit');
+// Route::put('/backpanel/users/{user}/edit', [App\Http\Controllers\User\UserController::class, 'update'])->name('user.update');
+// Route::delete('/backpanel/users/{user}/edit', [App\Http\Controllers\User\UserController::class, 'destroy'])->name('user.destroy');
+
+Route::resource('backpanel/user', UserController::class);
