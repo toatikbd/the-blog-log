e91348012db0e9be4bc32ede8faacd72c59e5fc8
Atiqur Rahaman <toatikbd@gmail.com>
2021-08-22 17:09:23 +0600

    create category api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Category;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//categories api // get all data
+Route::get('/categories', function (){
+    return Category::all();
 });
